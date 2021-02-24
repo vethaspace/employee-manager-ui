6846db5cf079a7cc7766a5ee7f28c8caae1a7061
vethaspace <vethaspace@gmail.com>
2021-02-23 23:55:39 -0500

    Service draft

---
+++ src/app/employee.service.ts
@@ -0,0 +1,15 @@
+import {HttpClient} from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import {Observable} from 'rxjs';
+import {Employee} from './employee';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class EmployeeService {
+    private apiServerUrl = '';
+
+    constructor(private http: HttpClient){}
+
+    
+}
