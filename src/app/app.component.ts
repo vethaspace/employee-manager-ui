6dedf50489d002ef6cd012ab178a3b08a639fdcb
Balasubramanian <mugilbala@gmail.com>
2021-02-25 01:43:58 -0500

    Added service class in UI component. Employee display changes. Cors filter changes.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Employee } from './employee';
+import { EmployeeService } from './employee.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'employee-manager-ui';
+export class AppComponent implements OnInit {
+  
+  public employees: Employee[];
+
+  constructor(private employeeService : EmployeeService){}
+  
+  ngOnInit() {
+    this.getEmployees();
+  }
+  
+  public getEmployees(): void{
+    this.employeeService.getEmployees().subscribe(
+      (response: Employee[]) => {
+        this.employees = response;
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+
+    );
+  }
+
+  public onOpenModal(employee: Employee, mode: string): void {
+    const button = document.createElement('button');
+    button.type = 'button';
+    button.style.display = 'none';
+    button.setAttribute('data-toggle', 'modal');
+    if (mode === 'add') {
+      button.setAttribute('data-target', '#addEmployeeModal');
+    } 
+    if (mode === 'edit') {
+      button.setAttribute('data-target', '#updateEmployeeModal');
+    } 
+    if (mode === 'delete') {
+      button.setAttribute('data-target', '#deleteEmployeeModal');
+    } 
+  }
 }
